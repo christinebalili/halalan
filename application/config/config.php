4ad7505d65051e419002265dd0cb0595eeba5bcf
Waldemar Bautista <waldemarbautista@gmail.com>
2012-03-14 00:47:58 +0800

    Change some config values to be default in Halalan

--- application/config/config.php
+++ application/config/config.php
@@ -244,8 +244,8 @@ $config['encryption_key'] = '';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'halalan_session';
-$config['sess_expiration']		= 7200;
+$config['sess_cookie_name']		= 'halalan_session_cookie_name';
+$config['sess_expiration']		= 1800;
 $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
@@ -293,10 +293,10 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
-$config['csrf_expire'] = 7200;
+$config['csrf_protection'] = TRUE;
+$config['csrf_token_name'] = 'halalan_csrf_token_name';
+$config['csrf_cookie_name'] = 'halalan_csrf_cookie_name';
+$config['csrf_expire'] = 1800;
 
 /*
 |--------------------------------------------------------------------------
