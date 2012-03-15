0344da1e48d4253817fcbc3f4197d72f870183b7
Waldemar Bautista <waldemarbautista@gmail.com>
2012-03-15 16:00:05 +0800

    Change Halalan config from array to string

--- application/config/halalan.php
+++ application/config/halalan.php
@@ -1,18 +1,16 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 // don't change if you already entered some data
-$config['halalan']['pin'] = TRUE;
-$config['halalan']['password_pin_generation'] = "web";
-$config['halalan']['password_pin_characters'] = "alnum";
-$config['halalan']['password_length'] = 4;
-$config['halalan']['pin_length'] = 4;
-$config['halalan']['captcha'] = TRUE;
-$config['halalan']['captcha_length'] = 4;
-$config['halalan']['show_candidate_details'] = TRUE;
-$config['halalan']['generate_image_trail'] = TRUE;
-$config['halalan']['image_trail_path'] = "/home/waldemar/public_html/htwo/trails/";
+$config['halalan_pin'] = TRUE;
+$config['halalan_password_pin_generation'] = "web";
+$config['halalan_password_pin_characters'] = "alnum";
+$config['halalan_password_length'] = 4;
+$config['halalan_pin_length'] = 4;
+$config['halalan_captcha'] = TRUE;
+$config['halalan_captcha_length'] = 4;
+$config['halalan_show_candidate_details'] = TRUE;
+$config['halalan_generate_image_trail'] = TRUE;
+$config['halalan_image_trail_path'] = "/home/waldemar/public_html/htwo/trails/";
 
-$config['base_url'] = "http://localhost/~waldemar/htwo/";
-$config['encryption_key'] = "oAnn954IRRetz3xDtPOp3ePdLdLNGHh4";
-
-?>
+/* End of file halalan.php */
+/* Location: ./application/config/halalan.php */
