a047c0794f8d3d40b3ed82cc9243f8dcd62e8277
Waldemar Bautista <waldemarbautista@gmail.com>
2012-03-16 20:02:35 +0800

    Update email configuration file

--- application/config/email.php
+++ application/config/email.php
@@ -1,12 +1,17 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['protocol'] = 'smtp'; // mail, sendmail, or smtp
+$config['newline'] = "\r\n";
+$config['crlf'] = "\r\n";
+
 // only needed if sendmail was chosen
 $config['mailpath'] = '/usr/sbin/sendmail';
+
 // only needed if smtp was chosen
-$config['smtp_host'] = '';
+$config['smtp_host'] = ''; // ssl://smtp.googlemail.com to use Gmail
 $config['smtp_user'] = '';
 $config['smtp_pass'] = '';
-$config['smtp_port'] = 25;
+$config['smtp_port'] = 25; // 465 for SSL
 
-?>
+/* End of file email.php */
+/* Location: ./application/config/email.php */
