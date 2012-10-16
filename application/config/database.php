be321038ed653e4b66fb2422336d3bfdcef28806
Waldemar Bautista <waldemarbautista@gmail.com>
2012-10-16 21:46:27 +0800

    Change default database name to halalan

--- application/config/database.php
+++ application/config/database.php
@@ -57,7 +57,7 @@ $active_record = TRUE;
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
 $db['default']['password'] = "";
-$db['default']['database'] = "htwo";
+$db['default']['database'] = "halalan";
 
 /*
 |===============================================================
